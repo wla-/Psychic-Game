6553e39a4350a44f32750b000d172a7266c3d485
William Alt <williamalt@WLAs-MacBook-Air.local>
2017-09-12 15:48:39 -0500

    updates to html and js files that track guesses remaining and reset after nine guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,56 +6,53 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-
 // computer randomly chooses from the options array
-// var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-// while (guesses > 0) {
-  // document.onkeyup = function(event) {
-	// var userGuess = event.key;
-  // }
-  // if (userGuess === computerGuess) {
-  	// wins = wins + 1;
-  // } else {
-  	// guesses = guesses - 1;
-  // }
-// }
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+// create variable to store user guessed letters in an array
+var guessedLetters = [];
 
+
+// function triggered by user keyup
 document.onkeyup = function(event) {
 	var userGuess = event.key;
-	var guessedLetters = [];
-            guessedLetters.push(userGuess);
-	console.log(userGuess);
+    guessedLetters.push(userGuess);
 	
+	console.log(userGuess);
 	console.log(computerGuess);
-
 	console.log(guessedLetters);
 
+// if userguess equals the random computer guess, you win
 	if (userGuess === computerGuess) {
+		// increment wins by 1
 		wins++;
+		// computer guesses a new letter
+		computerGuess = alphabet[Math.floor(Math.random() *alphabet.length)];
+		// reset guesses to nine
+		guesses = 9;
+		// reset the guessed letters
+		guessedLetters = [];
 	}  else if (userGuess !== computerGuess) {
+		// decrement guesses remaining
 		guesses = guesses -1;
-	}  
-
-	if (guesses === 0) {
+	}  if (guesses === 0) {
+		// increment losses by 1
 		losses++;
+		// computer guesses a new letter
+		computerGuess = alphabet[Math.floor(Math.random() *alphabet.length)];
+		// reset guesses to nine
+		guesses = 9;
+		// reset the guessed letters
+		guessedLetters = [];
 	}
 
+// dynamic updates to innerHTML
 	var html = 
-	"<p>wins: " + wins + "</p>" +
-	"<p>losses: " + losses + "</p>" +
-	"<p>guesses left: " + guesses + "</p>" +
-	"<p>your guesses so far: " + guessedLetters + "</p>"; 
+	"<p>Wins: " + wins + "</p>" +
+	"<p>Losses: " + losses + "</p>" +
+	"<p>Guesses Left: " + guesses + "</p>" +
+	"<p>Your Guesses So Far: " + guessedLetters + "</p>"; 
 
 	document.querySelector("#game").innerHTML = html;
 }
-// function for user guessing a letter (by pressing a key)
 
-
-
-
-// logic determining outcome of game
-
-// create variable to hold new html to track user guesses, wins, losses
 
